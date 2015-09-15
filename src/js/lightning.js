025818f6c1b56d33dbc4ef1c0aeb168ce550b3b6
Amir <amir@fb.com>
2015-09-15 17:52:13 +0300

    Added lightning forks.

--- src/js/lightning.js
+++ src/js/lightning.js
@@ -8,7 +8,7 @@ import Line from './line.js';
 const TAO = Math.PI * 2;
 
 const defaults = {
-  minSegmentLength: 6,
+  minSegmentLength: 10,
   distanceFalloff: 0.4,
   angleConstraint: TAO * 0.4,
   lineStartSize: 1.4,
@@ -19,7 +19,9 @@ const defaults = {
   lineEndOpacity: 0.2,
   glowColor: '#d471e8',
   glowBlur: 5,
-  hitRadius: 10
+  hitRadius: 10,
+  forkProbability: 0.02,
+  forkLength: 150
 };
 
 export default class Lightning {
@@ -37,6 +39,12 @@ export default class Lightning {
     this.points = [line.point1, line.point2];
 
     this.points = this.subdivide(this.points, line);
+
+    this.colorFn = interpolateLab(this.options.lineStartColor, this.options.lineEndColor);
+    this.sizeFn = lerp.bind(this, this.options.lineStartSize, this.options.lineEndSize);
+    this.opacityFn = lerp.bind(this, this.options.lineStartOpacity, this.options.lineEndOpacity);
+
+    this.forks = this.createForks();
   }
 
   render(ctx) {
@@ -50,16 +58,12 @@ export default class Lightning {
 
     const pointCount = this.points.length;
 
-    const colorFn = interpolateLab(this.options.lineStartColor, this.options.lineEndColor);
-
     this.points.slice(1).forEach((p, i) => {
-      const t = i / pointCount,
-            size = lerp(this.options.lineStartSize, this.options.lineEndSize, t),
-            opacity = lerp(this.options.lineStartOpacity, this.options.lineEndOpacity, t);
+      const t = i / pointCount;
 
-      ctx.globalAlpha = opacity;
-      ctx.lineWidth = size;
-      ctx.strokeStyle = colorFn(t);
+      ctx.globalAlpha = this.opacityFn(t);
+      ctx.lineWidth = this.sizeFn(t);
+      ctx.strokeStyle = this.colorFn(t);
 
       ctx.lineTo(p.x, p.y);
 
@@ -71,6 +75,8 @@ export default class Lightning {
     });
 
     ctx.restore();
+
+    this.forks.forEach(l => l.render(ctx));
   }
 
   subdivide(points, line) {
@@ -83,7 +89,7 @@ export default class Lightning {
     const center = line.center();
 
     const direction = line.direction() + (Math.random() - 0.5) * this.options.angleConstraint,
-          distance = lineLength * (Math.random() * this.options.distanceFalloff);
+          distance = lineLength * (0.1 + Math.random() * 0.9) * this.options.distanceFalloff;
 
     const offset = new Vector(
       Math.cos(direction) * distance,
@@ -101,6 +107,44 @@ export default class Lightning {
       new Line(points[1].x, points[1].y, points[2].x, points[2].y)
     );
 
-    return half1.concat(half2);
+    return half1.concat(half2.slice(1)); // half2's first point is the same as half1'x last point, we don't want that duplication.
+  }
+
+  createForks() {
+
+    const forks = [];
+
+    if (this.options.forkProbability === null) {
+      return forks;
+    }
+
+    this.points.slice(1).forEach((p, i) => {
+      if (Math.random() > this.options.forkProbability) {
+        return;
+      }
+
+      const t = i / this.points.length;
+
+      const currentDirection = this.points[i] // i already points to the previous point, no need to subtract.
+        .subtract(p)
+        .direction();
+
+      const distance = this.options.forkLength * 0.2 + Math.random() * this.options.forkLength * 0.8,
+            direction = currentDirection + (Math.random() - 0.5) * this.options.angleConstraint;
+
+      const x2 = p.x + Math.cos(direction) * distance,
+            y2 = p.y + Math.sin(direction) * distance;
+
+      forks.push(new Lightning(p.x, p.y, x2, y2, {
+        lineStartSize: this.sizeFn(t),
+        lineStartColor: this.colorFn(t),
+        lineStartOpacity: this.opacityFn(t),
+        lineEndSize: 0,
+        glowBlur: this.options.glowBlur * 0.8,
+        forkProbability: null
+      }));
+    });
+
+    return forks;
   }
 }
